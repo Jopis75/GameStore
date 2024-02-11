53c3e27bb7b55f0f435dabe62107c0f8c6468941
Johan Steinrud <johan.steinrud@gmail.com>
2024-02-11 13:50:22 +0100

    Added unique check validatioh for CreateAddressRequestDto and UpdateAddressRequestDto.

--- Application/Validators/Addresses/CreateAddressRequestDtoValidator.cs
+++ Application/Validators/Addresses/CreateAddressRequestDtoValidator.cs
@@ -1,20 +1,31 @@
 ﻿using Application.Dtos.Addresses;
+using Application.Interfaces.Persistance;
 using FluentValidation;
 
 namespace Application.Validators.Addresses
 {
+    //LINE 1: ADDRESSEE'S FULL LEGAL NAME
+    //LINE 2: STREET ADDRESS OR POST OFFICE BOX NUMBER
+    //LINE 3: CITY OR TOWN NAME, OTHER PRINCIPAL SUBDIVISION(i.e.PROVINCE, STATE, COUNTY, ETC.) AND POSTAL CODE(IF KNOWN) (Note: in some countries, the postal code may precede the city or town name)
+    //LINE 4: COUNTRY NAME(UPPERCASE LETTERS IN ENGLISH)
     public class CreateAddressRequestDtoValidator : AbstractValidator<CreateAddressRequestDto>
     {
-        //LINE 1: ADDRESSEE'S FULL LEGAL NAME
-        //LINE 2: STREET ADDRESS OR POST OFFICE BOX NUMBER
-        //LINE 3: CITY OR TOWN NAME, OTHER PRINCIPAL SUBDIVISION(i.e.PROVINCE, STATE, COUNTY, ETC.) AND POSTAL CODE(IF KNOWN) (Note: in some countries, the postal code may precede the city or town name)
-        //LINE 4: COUNTRY NAME(UPPERCASE LETTERS IN ENGLISH)
-        public CreateAddressRequestDtoValidator()
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CreateAddressRequestDtoValidator(IUnitOfWork unitOfWork)
         {
+            _unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+
             RuleFor(createAddressRequestDto => createAddressRequestDto.StreetAddress)
                 .NotNull()
                 .NotEmpty()
-                .WithMessage("{PropertyName} is required.");
+                .WithMessage("{PropertyName} is required.")
+                .MustAsync(async (streetAddress, cancellation) =>
+                {
+                    var address = await _unitOfWork.AddressRepository.ReadByStreetAddressAsync(streetAddress);
+                    return address.Id == 0;
+                })
+                .WithMessage("{PropertyName} must be unique.");
 
             RuleFor(createAddressRequestDto => createAddressRequestDto.City)
                 .NotNull()
--- Application/Validators/Addresses/UpdateAddressRequestDtoValidator.cs
+++ Application/Validators/Addresses/UpdateAddressRequestDtoValidator.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Dtos.Addresses;
+using Application.Interfaces.Persistance;
 using Application.Validators.Common;
 using FluentValidation;
 
@@ -6,14 +7,24 @@ namespace Application.Validators.Addresses
 {
     public class UpdateAddressRequestDtoValidator : AbstractValidator<UpdateAddressRequestDto>
     {
-        public UpdateAddressRequestDtoValidator()
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UpdateAddressRequestDtoValidator(IUnitOfWork unitOfWork)
         {
+            _unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+
             Include(new UpdateRequestDtoValidator());
 
             RuleFor(updateAddressRequestDto => updateAddressRequestDto.StreetAddress)
                 .NotNull()
                 .NotEmpty()
-                .WithMessage("{PropertyName} is required.");
+                .WithMessage("{PropertyName} is required.")
+                .MustAsync(async (streetAddress, cancellation) =>
+                {
+                    var address = await _unitOfWork.AddressRepository.ReadByStreetAddressAsync(streetAddress);
+                    return address.Id == 0;
+                })
+                .WithMessage("{PropertyName} must be unique.");
 
             RuleFor(updateAddressRequestDto => updateAddressRequestDto.City)
                 .NotNull()

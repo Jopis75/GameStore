aa7358e13b80a3760b862b9d400e1005f2a22055
Johan Steinrud <johan.steinrud@gmail.com>
2025-03-03 14:01:40 +0100

    Added code to GameStoreFielService.UpsertAsync.

--- Infrastructure/Services/GameStoreFileService.cs
+++ Infrastructure/Services/GameStoreFileService.cs
@@ -17,6 +17,24 @@ namespace Infrastructure.Services
         {
             var memoryStream = new MemoryStream();
             await formFile.CopyToAsync(memoryStream, cancellationToken);
+            memoryStream.Position = 0;
+
+            TextReader textReader = new StreamReader(memoryStream);
+            var rows = (await textReader.ReadToEndAsync()).Split("\r\n");
+
+            foreach (var row in rows)
+            {
+                var columns = row.Split('|');
+
+                var videoGameName = columns[0].Trim();
+                var consoleName = columns[1].Trim();
+                var developerName = columns[2].Trim();
+                var genreName = columns[3].Trim();
+                var videoGameReleaseDate = DateTime.Parse(columns[4].Trim());
+                var videoGamePurchaseDate = DateTime.Parse(columns[5].Trim());
+                var videoGamePrice = Decimal.Parse(columns[6].Trim());
+            }
+
         }
 
         public Task UpsertAsync(Stream stream)

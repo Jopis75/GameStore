a6a136cc9a770ace8e905ca3ee9dcc31d18d885f
Johan Steinrud <johan.steinrud@gmail.com>
2026-06-24 20:17:28 +0200

    Added a generic IEventSourcingHandler interface and a AddressEventSourcingHandler class.

---
+++ Application/Interfaces/EventStore/IEventSourcingHandler.cs
@@ -0,0 +1,11 @@
+﻿using Application.Aggregates;
+
+namespace Application.Interfaces.EventStore
+{
+    public interface IEventSourcingHandler<TAggregate> where TAggregate : AggregateRoot
+    {
+        Task<TAggregate> GetByIdAsync(Guid aggregateId);
+
+        Task SaveAsync(TAggregate aggregate);
+    }
+}
---
+++ EventStore/Handlers/Addresses/AddressEventSourcingHandler.cs
@@ -0,0 +1,18 @@
+﻿using Application.Aggregates.Addresses;
+using Application.Interfaces.EventStore;
+
+namespace EventStore.Handlers.Addresses
+{
+    public class AddressEventSourcingHandler(IEventStoreService eventStoreService) : IEventSourcingHandler<AddressAggregate>
+    {
+        public Task<AddressAggregate> GetByIdAsync(Guid aggregateId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveAsync(AddressAggregate aggregate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
